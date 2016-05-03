67575055c2e13049e3ba9208ec33585665997342
zyl <799429303@qq.com>
2016-05-03 23:36:25 +0800

    first

---
+++ Main/Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Main.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View("MainPage");
+        }
+
+        public string GetString()
+        {
+            return "hello world";
+        }
+
+    }
+}
