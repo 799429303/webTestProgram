29ce197eecee2ac837debcd51e6e24f99b4002f6
799429303@qq.com <799429303@qq.com>
2016-05-05 17:19:54 +0800

    添加Grid

---
+++ Main/ViewModels/TP_ListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Main.ViewModels
+{
+    public class TP_ListViewModel
+    {
+        public string TP_NO { get; set; }
+        public string TP_Name { get; set; }
+        public int TP_Type { get; set; }
+    }
+
+
+}
