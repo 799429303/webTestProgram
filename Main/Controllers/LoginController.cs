3a689f74037bf78595f37a914ad8df0af3ea09a7
799429303@qq.com <799429303@qq.com>
2016-05-18 18:08:24 +0800

    登录后台代码

--- Main/Controllers/LoginController.cs
+++ Main/Controllers/LoginController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using DAL;
 namespace Main.Controllers
 {
     public class LoginController : Controller
@@ -13,5 +13,21 @@ namespace Main.Controllers
         {
             return View("Login");
         }
+
+        public bool UserVer(string UserName, string Password)
+        {
+            SQLHelper sqlHelper = new SQLHelper();
+            System.Text.StringBuilder sbSql = new System.Text.StringBuilder();
+            sbSql.Append("select count(*) from XM_User");
+            sbSql.Append(" where UserName=@UserName and Password=@Password");
+            
+            int intResult=int.Parse(sqlHelper.ExecuteScalar(sbSql.ToString()));
+            if (intResult == 1)
+                return true;
+            else
+                return false;
+        }
     }
+
+   
 }
